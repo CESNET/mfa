898a638a428b63979bd6e83a6c7945a7ab88cd1a
Jan Oppolzer <jan@oppolzer.cz>
2024-08-14 11:05:52 +0200

    Fix cleanup task in Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -129,6 +129,6 @@
 
 @task('cleanup')
     cd {{ $dir }}
-    find . -maxdepth 1 -name "20*" | sort | head -n -3 | xargs rm -rf
+    find . -maxdepth 1 -name "mfa*" | sort | head -n -6 | xargs rm -rf
     echo "Cleaned up all but the last 3 deployments."
 @endtask
